6645bac129466cc61c8b46b2b319d255b8035463
Huanghaotian <1845720928@qq.com>
2020-07-23 00:21:09 +0800

    init

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,86 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <main v-show="this.data !== null">
+      <div id="firstLine">{{month}} 月 {{day}} 日</div>
+      <div id="secondLine">{{hour}} : {{minute}} : {{second}}</div>
+    </main>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+  export default {
+    data() {
+      return {
+        data: null,
+        month: null,
+        day: null,
+        hour: null,
+        minute: null,
+        second: null
+      }
+    },
+    methods: {
+      getTime() {
+        const init = () => {
+          getDate()
+          getHours()
+          getMonth()
+          getDay()
+          getMinute()
+          getSecond()
+        }
+        const getDate = () => {
+          this.data = new Date()
+        }
+        const getMonth = () => {
+          this.month = this.data.getMonth()
+        }
+        const getDay = () => {
+          this.day = this.data.getDay()
+        }
+        const getHours = () => {
+          this.hour = this.data.getHours()
+        }
+        const getMinute = () => {
+          this.minute = this.data.getMinutes()
+        }
+        const getSecond = () => {
+          this.second = this.data.getSeconds()
+        }
+        init()
+      }
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+    },
+    created() {
+      setInterval(() => {
+        this.getTime()
+      }, 1000)
+    }
   }
-}
 </script>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style>
+
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  main {
+    width: 100vw;
+    height: 100vh;
+    background: black;
+    color: white;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+    font-size: 3rem;
+  }
+
+  #firstLine {
+    margin-bottom: 20px;
+  }
+
 </style>
